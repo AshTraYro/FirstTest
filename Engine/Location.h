84aa11b95757e5ae9a27dde3ea58e599453cea9c
Oana <Oana@DESKTOP-RDSUDP1>
2022-07-08 00:35:50 +0300

    Add snake

---
+++ Engine/Location.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class  Location
+{
+public:
+	void Add(const Location& val)
+	{
+		x = x + val.x;
+		y = y + val.y;
+	}
+	bool operator ==(const Location& rhs) const
+	{
+		return x == rhs.x && y == rhs.y;
+	}
+	bool operator !=(const Location& rhs1) const
+	{
+		return x != rhs1.x && y != rhs1.y;
+	}
+	int x;
+	int y;
+};
